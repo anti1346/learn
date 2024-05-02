ab16264e63829573b268e61dc8a0ba7b07e11a1b
anti1346 <anti1346@mt.co.kr>
2024-05-02 11:13:29 +0900

    update Thu May  2 11:13:29 AM KST 2024

--- python/mkdir.py
+++ python/mkdir.py
@@ -1,5 +1,11 @@
 import os
 
+# 디렉토리 경로 설정
 directory_path = '/tmp/python'
 
-os.mkdir(directory_path)
+# 디렉토리가 존재하지 않는 경우에만 생성
+if not os.path.exists(directory_path):
+    os.mkdir(directory_path)
+    print("디렉토리가 생성되었습니다.")
+else:
+    print("이미 디렉토리가 존재합니다.")
