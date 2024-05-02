3a1d6e0abea96100e57c9bf9ec419d41272b8fb4
anti1346 <anti1346@mt.co.kr>
2024-05-02 13:19:42 +0900

    Update 20240502-131942

--- git_commit.sh
+++ git_commit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# 현재 시간을 변수에 저장
+current_time=$(date '+%Y%m%d-%H%M%S')
+
+# 변경 사항을 추가하고 커밋
 git add .
 
-git commit -m "update $(date)"
+git commit -m "Update $current_time"
 
+# 변경 사항을 푸시
 git push origin main
