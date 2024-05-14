590c25eac02f7067891fa84ca817d6ab6cbbb53a
anti1346 <anti1346@mt.co.kr>
2024-05-14 11:05:37 +0900

    no message

--- python/bind_install_configure.py
+++ python/bind_install_configure.py
@@ -11,6 +11,135 @@ def get_linux_distribution():
     os_info = distro.id()
     return os_info
 
+# 운영 체제 판단 및 업데이트
+distro = get_linux_distribution()
+
+if distro == "ubuntu":
+    print(f"이 스크립트는 {distro.capitalize()}만 지원합니다.")
+else:
+    print(f"이 스크립트는 {distro.capitalize()}를 지원하지 않습니다.")
+    exit(1)
+
+update_result = subprocess.run(['sudo', 'apt-get', 'update'])
+if update_result.returncode != 0:
+    print(f"APT 업데이트 실패")
+    exit(1)
+
+# 빌드 필수 패키지 설치
+required_packages = ['wget', 'build-essential', 'pkg-config', 'make']
+
+for package in required_packages:
+    install_result = subprocess.run(['sudo', 'apt-get', 'install', '-y', package])
+    if install_result.returncode != 0:
+        print(f"빌드 필수 패키지 '{package}' 설치 실패")
+        exit(1)
+
+# BIND 필수 패키지 설치
+required_packages = ['libssl-dev', 'libuv1-dev', 'libcap-dev', 'libjemalloc2', 'libjemalloc-dev', 'libnghttp2-dev']
+
+for package in required_packages:
+    install_result = subprocess.run(['sudo', 'apt-get', 'install', '-y', package])
+    if install_result.returncode != 0:
+        print(f"BIND 필수 패키지 '{package}' 설치 실패")
+        exit(1)
+
+# named 계정이 있는지 확인
+check_user_command = subprocess.run(['getent', 'passwd', 'named'], capture_output=True, text=True)
+if check_user_command.returncode == 0:
+    print("named 계정이 이미 존재합니다.")
+else:
+    # 사용자 및 그룹 추가
+    subprocess.run(['sudo', 'adduser', '--system', '--home', '/var/named', '--no-create-home', '--disabled-login', '--disabled-password', '--group', 'named'])
+    print("named 계정을 추가했습니다.")
+
+# 디렉토리1 이동
+change_dir_result = os.chdir('/usr/local/src')
+if change_dir_result is None:
+    print("/usr/local/src 디렉토리 변경 성공")
+else:
+    print("/usr/local/src 디렉토리 변경 실패")
+    exit(1)
+
+# 소스코드 다운로드 및 압축 해제
+download_result = subprocess.run(['sudo', 'wget', f'https://ftp.isc.org/isc/bind9/{bind_version}/bind-{bind_version}.tar.xz'])
+if download_result.returncode != 0:
+    print("BIND 패키지 다운로드 실패")
+    exit(1)
+
+extract_result = subprocess.run(['tar', '-xf', f'bind-{bind_version}.tar.xz'])
+if extract_result.returncode != 0:
+    print("압축 해제 실패")
+    exit(1)
+
+# 디렉토리2 이동
+change_dir_result = os.chdir(f'bind-{bind_version}')
+if change_dir_result is None:
+    print(f"bind-{bind_version} 디렉토리 변경 성공")
+else:
+    print(f"bind-{bind_version} 디렉토리 변경 실패")
+    exit(1)
+
+# 빌드(configure) 및 컴파일
+configure_result = subprocess.run(['./configure', f'--prefix=/usr/local/named', '--sysconfdir=/etc', '--localstatedir=/var', '--with-openssl'])
+if configure_result.returncode != 0:
+    print("빌드(configure) 실패")
+    exit(1)
+
+make_result = subprocess.run(['make', '-j', str(int(subprocess.check_output(['nproc'])) // 2)])
+if make_result.returncode != 0:
+    print("컴파일 실패")
+    exit(1)
+
+# 설치
+install_result = subprocess.run(['sudo', 'make', 'install'])
+if install_result.returncode != 0:
+    print("설치 실패")
+    exit(1)
+
+# symbolic link 생성
+subprocess.run(['ln', '-s', '/usr/local/named/bin/named-checkconf', '/bin/named-checkconf'])
+subprocess.run(['ln', '-s', '/usr/local/named/bin/named-checkzone', '/bin/named-checkzone'])
+subprocess.run(['ln', '-s', '/usr/local/named/sbin/named', '/sbin/named'])
+subprocess.run(['ln', '-s', '/usr/local/named/sbin/named', '/sbin/rndc'])
+
+# # named.conf 파일 작성
+# named_conf_content = """
+# options {
+#   directory "/var/named";
+#   version "not currently available";
+# };
+
+# logging {
+#   channel example_log {
+#     file "log/example.log" versions 3 size 250k;
+#     severity info;
+#   };
+#   category default {
+#     example_log;
+#   };
+# };
+# """
+# with open('/etc/named.conf', 'w') as file:
+#     file.write(named_conf_content)
+
+# data, log 디렉토리 생성
+# 디렉토리 생성 함수
+def create_directory(directory):
+    try:
+        os.makedirs(directory, exist_ok=True)
+        print(f"{directory} 디렉토리 생성 완료")
+    except OSError as e:
+        print(f"{directory} 디렉토리 생성 실패: {e}")
+        exit(1)
+
+# data, log 디렉토리 생성
+create_directory('/var/named/data')
+create_directory('/var/named/log')
+
+# 라이브러리 경로를 ld.so.conf 파일에 추가
+with open('/etc/ld.so.conf', "a") as file:
+    file.write('/usr/local/named/lib' + "\n")
+
 def download_file(url, destination):
     # 파일 다운로드 요청
     response = requests.get(url)
@@ -23,127 +152,6 @@ def download_file(url, destination):
         print(f"다운로드 완료: {destination}")
     else:
         print(f"파일 다운로드 실패: {response.status_code}")
-
-# 운영 체제 판단 및 업데이트
-distro = get_linux_distribution()
-
-if distro == "ubuntu":
-    print(f"이 스크립트는 {distro.capitalize()}만 지원합니다.")
-else:
-    print(f"이 스크립트는 {distro.capitalize()}를 지원하지 않습니다.")
-    exit(1)
-
-# update_result = subprocess.run(['sudo', 'apt-get', 'update'])
-# if update_result.returncode != 0:
-#     print(f"APT 업데이트 실패")
-#     exit(1)
-
-# # 빌드 필수 패키지 설치
-# required_packages = ['wget', 'build-essential', 'pkg-config', 'make']
-
-# for package in required_packages:
-#     install_result = subprocess.run(['sudo', 'apt-get', 'install', '-y', package])
-#     if install_result.returncode != 0:
-#         print(f"빌드 필수 패키지 '{package}' 설치 실패")
-#         exit(1)
-
-# # BIND 필수 패키지 설치
-# required_packages = ['libssl-dev', 'libuv1-dev', 'libcap-dev', 'libjemalloc2', 'libjemalloc-dev', 'libnghttp2-dev']
-
-# for package in required_packages:
-#     install_result = subprocess.run(['sudo', 'apt-get', 'install', '-y', package])
-#     if install_result.returncode != 0:
-#         print(f"BIND 필수 패키지 '{package}' 설치 실패")
-#         exit(1)
-
-# # # 사용자 및 그룹 추가
-# # subprocess.run(['sudo', 'adduser', '--system', '--home', '/var/named', '--no-create-home', '--disabled-login', '--disabled-password', '--group', 'named'])
-# # named 계정이 있는지 확인
-# check_user_command = subprocess.run(['getent', 'passwd', 'named'], capture_output=True, text=True)
-# if check_user_command.returncode == 0:
-#     print("named 계정이 이미 존재합니다.")
-# else:
-#     # 사용자 및 그룹 추가
-#     subprocess.run(['sudo', 'adduser', '--system', '--home', '/var/named', '--no-create-home', '--disabled-login', '--disabled-password', '--group', 'named'])
-#     print("named 계정을 추가했습니다.")
-
-# # 디렉토리1 이동
-# change_dir_result = os.chdir('/usr/local/src')
-# if change_dir_result is None:
-#     print("/usr/local/src 디렉토리 변경 성공")
-# else:
-#     print("/usr/local/src 디렉토리 변경 실패")
-#     exit(1)
-
-# # 소스코드 다운로드 및 압축 해제
-# download_result = subprocess.run(['sudo', 'wget', f'https://ftp.isc.org/isc/bind9/{bind_version}/bind-{bind_version}.tar.xz'])
-# if download_result.returncode != 0:
-#     print("BIND 패키지 다운로드 실패")
-#     exit(1)
-
-# extract_result = subprocess.run(['tar', '-xf', f'bind-{bind_version}.tar.xz'])
-# if extract_result.returncode != 0:
-#     print("압축 해제 실패")
-#     exit(1)
-
-# # 디렉토리2 이동
-# change_dir_result = os.chdir(f'bind-{bind_version}')
-# if change_dir_result is None:
-#     print(f"bind-{bind_version} 디렉토리 변경 성공")
-# else:
-#     print(f"bind-{bind_version} 디렉토리 변경 실패")
-#     exit(1)
-
-# # 빌드(configure) 및 컴파일
-# configure_result = subprocess.run(['./configure', f'--prefix=/usr/local/named', '--sysconfdir=/etc', '--localstatedir=/var', '--with-openssl'])
-# if configure_result.returncode != 0:
-#     print("빌드(configure) 실패")
-#     exit(1)
-
-# make_result = subprocess.run(['make', '-j', str(int(subprocess.check_output(['nproc'])) // 2)])
-# if make_result.returncode != 0:
-#     print("컴파일 실패")
-#     exit(1)
-
-# # 설치
-# install_result = subprocess.run(['sudo', 'make', 'install'])
-# if install_result.returncode != 0:
-#     print("설치 실패")
-#     exit(1)
-
-# # symbolic link 생성
-# subprocess.run(['ln', '-s', '/usr/local/named/bin/named-checkconf', '/bin/named-checkconf'])
-# subprocess.run(['ln', '-s', '/usr/local/named/bin/named-checkzone', '/bin/named-checkzone'])
-# subprocess.run(['ln', '-s', '/usr/local/named/sbin/named', '/sbin/named'])
-# subprocess.run(['ln', '-s', '/usr/local/named/sbin/named', '/sbin/rndc'])
-
-# named.conf 파일 작성
-named_conf_content = """
-options {
-  directory "/var/named";
-  version "not currently available";
-};
-
-logging {
-  channel example_log {
-    file "log/example.log" versions 3 size 250k;
-    severity info;
-  };
-  category default {
-    example_log;
-  };
-};
-"""
-with open('/etc/named.conf', 'w') as file:
-    file.write(named_conf_content)
-
-# data, log 디렉토리 생성
-subprocess.run(['mkdir', '-p', '/var/named/data'])
-subprocess.run(['mkdir', '-p', '/var/named/log'])
-
-# 라이브러리 경로를 ld.so.conf 파일에 추가
-with open('/etc/ld.so.conf', "a") as file:
-    file.write('/usr/local/named/lib' + "\n")
 
 # 다운로드할 파일과 저장할 경로 정의
 files = {
@@ -158,8 +166,17 @@ files = {
 for url, destination in files.items():
     download_file(url, destination)
 
-# data, log 디렉토리 권한 설정
-subprocess.run(['chown', 'named.named', '-R', '/var/named'])
+# 디렉토리 소유자 변경 함수
+def change_owner(directory, user, group):
+    try:
+        os.chown(directory, user, group)
+        print(f"{directory}의 소유자를 {user}.{group}으로 변경 완료")
+    except OSError as e:
+        print(f"{directory}의 소유자 변경 실패: {e}")
+        exit(1)
+
+# /var/named 디렉토리의 소유자 변경
+change_owner('/var/named', 'named', 'named')
 
 # systemd 서비스 파일 작성
 systemd_service_content = """
